a1841c95a2f7ebcbe60527725fa61dbbd03277a8
Guo Feng He <guofengh@nycstudents.net>
2023-10-09 10:36:20 -0400

    added some more rules

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,23 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         int score = 0;
-        int numberOfWords = 0;
+        int previousScore = 0;
+        int numberOfWords = 1;
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the first word: ");
         String word = scan.nextLine();
         while (score < 50){
             System.out.print("Enter another word: ");
             String currentWord = scan.nextLine();
-            
+
             if (currentWord.compareTo(word) > 0){
                 score += 2;
             }
             else if (currentWord.compareTo(word) == 0){
                 score -= 10;
+            }
+            else if (currentWord.indexOf(word) != -1){
+                score += 20;
             }
             else{
                 score -= 5;
@@ -27,9 +31,10 @@ public class Main{
                 score += 3;
             }
             word = currentWord;
-            System.out.println("Points: " + score);
+            System.out.println("Points: " + score + " (" + (score - previousScore) + " points)");
             numberOfWords ++;
+            previousScore = score;
         }
-        System.out.println("It took you " + numberOfWords + " to reach 50 points");
+        System.out.println("It took you " + numberOfWords + " words to reach 50 points");
     }
 }
